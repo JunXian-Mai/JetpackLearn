8ba09a0af3d70e078433a82678e58531f853c7d9
maijunxian <markensic@outlook.com>
2021-08-25 16:11:02 +0800

    替换核心类

--- app/src/main/java/org/markensic/learn/jetpack/App.kt
+++ app/src/main/java/org/markensic/learn/jetpack/App.kt
@@ -3,16 +3,14 @@ package org.markensic.learn.jetpack
 import android.content.Context
 import com.baidu.mapapi.CoordType
 import com.baidu.mapapi.SDKInitializer
-import com.markensic.core.global.App
-import com.markensic.core.global.stack.ActivityStack
+import com.markensic.core.global.CoreApp
 import org.markensic.mvvm.base.BaseMvvmApplication
 
 class App : BaseMvvmApplication() {
-    override val activityStack = ActivityStack()
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
-        App.initApplication(this)
+        CoreApp.initCore(this)
     }
 
     override fun onCreate() {
